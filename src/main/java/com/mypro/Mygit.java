9614769d9fe808adfe1c0900f27d45a088c67841
TH_guan <3375989217@qq.com>
2023-01-20 16:17:29 +0800

    push commit

--- src/main/java/com/mypro/Mygit.java
+++ src/main/java/com/mypro/Mygit.java
@@ -9,5 +9,6 @@ public class Mygit {
         System.out.println("hello git -hotfix");
         System.out.println("冲突1-master");
         System.out.println("冲突-1");
+        System.out.println("push test");
     }
 }

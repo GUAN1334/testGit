312fc7b71d1573bd78200d621c1de6e82b42e9ce
TH_guan <3375989217@qq.com>
2023-01-20 15:34:14 +0800

    hot-fix commit2

--- src/main/java/com/mypro/Mygit.java
+++ src/main/java/com/mypro/Mygit.java
@@ -7,5 +7,6 @@ public class Mygit {
     public static void main(String[] args) {
         System.out.println("hello git ");
         System.out.println("hello git -hotfix");
+        System.out.println("冲突-1");
     }
 }

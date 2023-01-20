a3df76cff9e3a95da2c3299326f79582c69f169a
GUAN1334 <92852845+GUAN1334@users.noreply.github.com>
2023-01-20 16:23:24 +0800

    pull commit

--- src/main/java/com/mypro/Mygit.java
+++ src/main/java/com/mypro/Mygit.java
@@ -10,5 +10,6 @@ public class Mygit {
         System.out.println("冲突1-master");
         System.out.println("冲突-1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
